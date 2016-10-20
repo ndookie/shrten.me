d489ef2072682c62207416a93f7643e31da35811
Nigel Dookie <ndookie@gmail.com>
2016-10-20 17:34:21 -0400

    First big commit with some general code and views. View needs finishing and all model code must be added.

---
+++ shorten.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('cover.html')
+
+@app.route('/shorten/', methods=['GET'])
+def shorten():
+	if request.method == 'GET':
+		continue
+	else:
+		render_template('cover.html', error="yes")
+
+
