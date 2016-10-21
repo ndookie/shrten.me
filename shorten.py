ab4c858aaddeb2d8a851b672a6f728dc5dcf8197
Nigel Dookie <ndookie@gmail.com>
2016-10-20 21:27:20 -0400

    UNTESTED CODE: more wokr done on main flask app.

--- shorten.py
+++ shorten.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 
+from url_shortener import url_generator
+from url_shortener import url_fetcher
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,8 +13,23 @@ def index():
 @app.route('/shorten/', methods=['GET'])
 def shorten():
 	if request.method == 'GET':
-		print "nigel"
+		url = request.args.get("full_url")
+
+		if url is None:
+			render_template('cover.html', error="yes")
+
+		new_hash = url_generator(url)
+		
+		#return render_template('cover.html', hash=new_hash)
+		print new_hash
 	else:
-		render_template('cover.html', error="yes")
+		return render_template('cover.html', error="yes")
 
+@app.route('/<url_hash>')
+def fetch_and_redirect(url_hash):
+	target_url = url_fetcher(url_hash)
 
+	if target_url is False:
+		return render_template('404.html')
+	else:
+		return redirect(target_url, code=302)
