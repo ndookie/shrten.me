573f96bec1d8c8f836c23f362c24a275ae6fa472
Nigel Dookie <ndookie@gmail.com>
2016-10-21 00:22:11 -0400

    Late night. Copy button not working. Shortener working.

--- shorten.py
+++ shorten.py
@@ -1,10 +1,86 @@
-from flask import Flask
-from flask import render_template
-
-from url_shortener import url_generator
-from url_shortener import url_fetcher
+from flask import Flask, render_template, request, url_for, g, redirect
+import sqlite3, sys, re
+from base_converter import base_conversion, hash_to_number
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+	g.db = sqlite3.connect("shortener.db")
+
+@app.teardown_request
+def teardown_request(exception):
+	if hasattr(g, 'db'):
+		g.db.close()
+
+
+base = 62
+
+# Function for inserting new URL into database.
+def insert_url(url):
+
+	con = g.db
+	cur = g.db.cursor()
+
+	url = re.sub('^https?://', '', url)
+
+	cur.execute("INSERT INTO url_table VALUES(NULL,'%s',NULL)" % (url))
+
+	cur.execute("SELECT id FROM url_table WHERE url='%s'" % (url));
+
+	url_id = cur.fetchall()
+
+	url_id = url_id[-1]
+
+	return url_id[0]
+
+# Generates hash based off id from table.
+def generate_hash(id):
+	new_hash = base_conversion(id,base)
+
+	return new_hash
+
+# Updates the entry for a specific ID with it's hash.
+def update_shortened_url(id, hash):
+	con = g.db
+	cur = g.db.cursor()
+
+	cur.execute("UPDATE url_table SET shortened_url='" + hash + "' WHERE id=%i" % (id));
+	con.commit()
+	return
+
+# Takes a url, inserts it into the database and returns the hash.
+def url_generator(url):
+	
+	new_id = insert_url(url)
+	shortened_url = generate_hash(new_id)
+	update_shortened_url(new_id, shortened_url)
+
+	return shortened_url
+
+# Fetches corresponding URL for hash from database.
+def url_fetcher(hash):
+	
+	fetching_id = hash_to_number(hash, base)
+#	with app.app_context():
+#		cur = get_db().cursor()
+
+	con = g.db
+	cur = g.db.cursor()
+
+	cur.execute("SELECT url FROM url_table WHERE id=" + str(fetching_id))
+	url = cur.fetchall()
+	url = url[-1]
+
+	if url[0] is None:
+		return False
+	else:
+		return url[0]
+
+
+
+
+#**********************ROUTES***********************
 
 @app.route('/')
 def index():
@@ -16,20 +92,21 @@ def shorten():
 		url = request.args.get("full_url")
 
 		if url is None:
-			render_template('cover.html', error="yes")
+			return render_template('cover.html', error="yes")
+			return
 
 		new_hash = url_generator(url)
 		
-		#return render_template('cover.html', hash=new_hash)
-		print new_hash
+		return render_template('cover.html', hash=new_hash)
 	else:
 		return render_template('cover.html', error="yes")
 
 @app.route('/<url_hash>')
 def fetch_and_redirect(url_hash):
+
 	target_url = url_fetcher(url_hash)
 
 	if target_url is False:
 		return render_template('404.html')
 	else:
-		return redirect(target_url, code=302)
+		return redirect("http://" + target_url)
